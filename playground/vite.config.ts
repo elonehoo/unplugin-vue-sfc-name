39fe6229fa2cef8b1999102ef8c5ee6d0694b39b
elonehoo <elonehoo@gmail.com>
2023-01-04 21:10:39 +0800

    fix: update vite config package name

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 // import Inspect from 'vite-plugin-inspect'
 import vue from '@vitejs/plugin-vue'
-import scriptSetupName from 'unplugin-script-setup-name/vite'
+import scriptSetupName from 'unplugin-vue-sfc-name/vite'
 
 export default defineConfig({
   plugins: [
