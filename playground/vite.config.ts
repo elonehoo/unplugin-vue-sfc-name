1945b13740678f7a4e1d7dc5d60b194d0a216627
elonehoo <elonehoo@gmail.com>
2023-01-12 12:36:07 +0800

    fix: remove vite config in ts error

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -6,7 +6,6 @@ import scriptSetupName from 'unplugin-vue-sfc-name/vite'
 export default defineConfig({
   plugins: [
     vue(),
-    // @ts-expect-error vite plugin error
     scriptSetupName(),
   ],
 })
